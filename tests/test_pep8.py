302dc91fdac7382e6db272435c4d43332af01bdb
Loïc Peron <peronloic.us@gmail.com>
2019-03-14 15:23:25 -0600

    fix pep8 test package name

--- tests/test_pep8.py
+++ tests/test_pep8.py
@@ -7,7 +7,7 @@ import pathlib
 def test_pep8_conformance():
     """Test that we conform to PEP8."""
     pep8style = pep8.StyleGuide(prefix='E')
-    base = pathlib.Path(simpy-events.__file__).parent
+    base = pathlib.Path(simpy_events.__file__).parent
     result = pep8style.check_files(map(str, [
         base,
         # base / 'subdir',
